1789f61902ca35ec0ddcafd3edf62f6a5be7cb02
unknown <waszka04@gmail.com>
2023-04-23 17:23:15 +0100

    add file

---
+++ helloword.py
@@ -0,0 +1,9 @@
+# Weekly Task 01
+# Author : Agnieszka Waszczuk 
+# messing with inputs 
+
+print("HelloWord")
+name = input("Enter your name: ")
+print (f" Hello {name} \n Nice to meet you {name}")
+
+
