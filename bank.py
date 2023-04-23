3c3e06c23a34a76bd94db1f6bbde18a74afe8a76
unknown <waszka04@gmail.com>
2023-04-23 11:11:12 +0100

    bank first push

---
+++ bank.py
@@ -0,0 +1,20 @@
+# Create two variables for the input values and convert input from string to integer
+amount1 = abs(int(input('Enter amount1(in cent)')))
+amount2 = abs(int(input('Enter amount2(in cent)')))
+
+# sum both amounts and convert numeric value to string
+total = str(amount1 + amount2)
+
+# use len() method to calculate position of decimal point
+length = len(total)
+
+if length >= 3:
+    output = '€'+total[:length-2]+'.'+total[-2:]
+elif length == 2:
+   output = '€'+total[:length-2]+'0.'+total[-2:]
+else:
+    output = '€'+total[:length-2]+'0.0'+total[-2:]
+
+
+# Print out the total with € sign in front of the number and 2 decimal places
+print(f'The sum of these is {output}')
